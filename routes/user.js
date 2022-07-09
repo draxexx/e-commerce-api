1e12f59ab1077bce4e51025b4cd26d3e80e79283
Burak <burak.yilmaz@simdatix.com>
2022-07-09 09:18:48 +0300

    add token verification

--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,33 @@ const express = require("express");
 // get Router function
 const router = express.Router();
 
+// get User model
+const User = require("../models/user.js");
+// import verifyTokenAndAuthorization middleware
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+
+// update user
+router.put("/:id", verifyTokenAndAuthorization, async (req, res, next) => {
+  // check there is a password in the body
+  // then encrypt the password
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  // find the user by id and update
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,34 @@
+// import json web token
+const jwt = require("jsonwebtoken");
+
+// to verify token
+const verifyToken = (req, res, next) => {
+  // get token from the headers
+  const authHeader = req.headers.token;
+  // if token exist
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) return res.status(403).json("Token is not valid!");
+      // create request user and assign user that comes from the callback func
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    // if request user id equals to request params id or user is admin
+    // request user id comes from verifyToken function, we created there
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(401).json("You are not allowed to do that");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
